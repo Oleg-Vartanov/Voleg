b3d004510b4631ff33391df66cf652a0b1a2d64a
Oleg Vartanov <o.vartanov@richbrains.net>
2024-08-10 01:04:23 +0400

    Added basic tests.

---
+++ backend/tests/AuthTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tests;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthTest extends WebTestCase
+{
+    private KernelBrowser $client;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    /** @test */
+    public function signUpSuccess(): void
+    {
+        /** @var UserRepository $userRepo */
+        $userRepo = $this->getContainer()->get(UserRepository::class);
+        $lastUserId = $userRepo->findOneBy([], ['id' => 'desc'])?->getId() ?? 1;
+
+        $response = $this->signUpRequest(json_encode([
+            'email' => 'user'.($lastUserId + 1).'@example.com',
+            'password' => '!Qwerty1',
+            'displayName' => 'John Doe',
+        ]));
+
+        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+    }
+
+    /** @test */
+    public function signUpValidationError(): void
+    {
+        $response = $this->signUpRequest(json_encode([
+            'email' => 'john.doe',
+            'password' => 'qwert y',
+            'displayName' => '',
+        ]));
+
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+    }
+
+    private function signUpRequest(string $bodyJson): Response
+    {
+        $headers = ['CONTENT_TYPE' => 'application/json'];
+        $this->client->request(method: 'POST', uri: '/sign-up/create', server: $headers, content: $bodyJson);
+
+        return $this->client->getResponse();
+    }
+}
