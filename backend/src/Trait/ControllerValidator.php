e48f4895b47d9de4454a4d7d2466151ebf60a04c
Oleg Vartanov <olegvartanov1997@gmail.com>
2024-11-07 21:59:11 +0400

    B: validationErrorResponse fix.

--- backend/src/Trait/ControllerValidator.php
+++ backend/src/Trait/ControllerValidator.php
@@ -4,6 +4,7 @@ namespace App\Trait;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints\GroupSequence;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 trait ControllerValidator
@@ -15,7 +16,7 @@ trait ControllerValidator
         return $this->validator->validate($value)->count() === 0;
     }
 
-    public function validationErrorResponse(mixed $value, $groups): ?JsonResponse
+    public function validationErrorResponse(mixed $value, string|GroupSequence|array|null $groups = null): ?JsonResponse
     {
         $errors = $this->validator->validate($value, groups: $groups);
         if ($errors->count() > 0) {
