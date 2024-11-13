d077474aa50097372ca1ad3959baafe689449839
Oleg Vartanov <olegvartanov1997@gmail.com>
2024-11-14 02:04:06 +0400

    F: Finished signUp. Added signIn verification error message.

--- frontend/src/views/SignInView.vue
+++ frontend/src/views/SignInView.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import client from '@/modules/api-client';
 import {useAuth} from '@/modules/auth';
-import {ref} from 'vue';
+import {type Ref, ref, type UnwrapRef} from 'vue';
 
 const auth = useAuth();
 
-const is401 = ref(false);
+const message401: Ref<UnwrapRef<string|null>> = ref(null);
 const isLoading = ref(false);
 
 const signIn = (event: SubmitEvent) => {
   isLoading.value = true;
-  is401.value = false;
+  message401.value = null;
 
   const formData = new FormData(event.target as HTMLFormElement);
   const formValues = Object.fromEntries(formData.entries());
@@ -19,8 +19,11 @@ const signIn = (event: SubmitEvent) => {
     .then((response) => {
       auth.signIn(response.data)
     })
-    .catch((response) => {
-      is401.value = true;
+    .catch((axiosError) => {
+      message401.value = 'Invalid credentials';
+      if (axiosError.response.status === 401 && axiosError.response.data.hasOwnProperty('message')) {
+        message401.value = axiosError.response.data.message;
+      }
     })
     .finally(() => {
       isLoading.value = false;
@@ -30,18 +33,24 @@ const signIn = (event: SubmitEvent) => {
 
 <template>
   <form @submit.prevent="signIn">
+
     <div class="form-floating mb-3">
-      <input name="email" type="email" class="form-control" id="floatingInput" placeholder="">
-      <label for="floatingInput">Email address</label>
+      <input name="email" type="email" class="form-control" id="email" placeholder="">
+      <label for="email">Email address</label>
     </div>
+
     <div class="form-floating mb-3">
-      <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="">
-      <label for="floatingPassword">Password</label>
+      <input name="password" type="password" class="form-control" id="password" placeholder="">
+      <label for="password">Password</label>
     </div>
+
     <button :disabled="isLoading" class="btn btn-primary w-100 py-2 mb-3" type="submit">Submit</button>
-    <div v-if="is401" class="alert alert-danger mb-3" role="alert">Invalid credentials</div>
+
+    <div v-if="message401 !== null" class="alert alert-danger mb-3" role="alert">{{ message401 }}</div>
+
     <div v-if="isLoading" class="spinner-border text-primary" role="status">
       <span class="visually-hidden">Loading...</span>
     </div>
+
   </form>
 </template>
--- frontend/src/views/SignUpView.vue
+++ frontend/src/views/SignUpView.vue
@@ -1,25 +1,127 @@
 <script setup lang="ts">
 import RouterHelper from '@/helpers/router-helper';
+import Client from '@/modules/api-client';
+import {type Router, useRouter} from 'vue-router';
+import {useTopAlerts} from '@/modules/top-alerts';
+import {ref, reactive} from 'vue';
+import {Alert} from "@/models/alert";
 
-const verificationEmailRedirectUrl = RouterHelper.url('signIn');
+const topAlerts = useTopAlerts();
+const router: Router = useRouter();
+
+const fields = reactive({
+  'displayName': { 'isValid': null, 'errorMessage': ''},
+  'email': { 'isValid': null, 'errorMessage': ''},
+  'password': { 'isValid': null, 'errorMessage': ''},
+});
+const isLoading = ref(false);
+
+const resetResult = () => {
+  Object.keys(fields).forEach((v) => {
+    fields[v]['isValid'] = null;
+    fields[v]['errorMessage'] = '';
+  })
+}
+
+const signUp = (event: SubmitEvent) => {
+  isLoading.value = true;
+  resetResult();
+
+  const formData = new FormData(event.target as HTMLFormElement);
+  const formValues = Object.fromEntries(formData.entries());
+
+  Client.signUp(formValues)
+    .then(() => {
+      topAlerts.add(new Alert('You\'ve signed up! All that\'s left is to verify your account via email.', 'success', 30));
+      router.push({ name: 'signIn' });
+    })
+    .catch((axiosError) => {
+      axiosError.response.data.violations.forEach(violation => {
+        const property = violation.propertyPath;
+        if (fields.hasOwnProperty(property)) {
+          fields[property].isValid = false;
+          fields[property].errorMessage += violation.title + "<br>";
+        }
+      });
+
+      Object.keys(fields).forEach((field) => {
+        if (fields[field]['isValid'] === null) {
+          fields[field]['isValid'] = true;
+        }
+      })
+    })
+    .finally(() => {
+      isLoading.value = false;
+    });
+}
 </script>
 
 <template>
-  <form>
-    <div class="form-floating">
-      <input type="text" class="form-control" id="floatingPassword" placeholder="">
-      <label for="floatingPassword">Display Name</label>
-    </div>
-    <div class="form-floating mt-3">
-      <input type="email" class="form-control" id="floatingInput" placeholder="">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating mt-3">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="">
-      <label for="floatingPassword">Password</label>
+  <form @submit.prevent="signUp">
+
+    <input name="verificationEmailRedirectUrl" hidden type="url" :value="RouterHelper.url('signIn')">
+
+    <div class="form-floating mb-3">
+      <input
+        name="displayName"
+        type="text"
+        class="form-control"
+        :class="[ fields.displayName.isValid === null ? '' : (fields.displayName.isValid ? 'is-valid' : 'is-invalid') ]"
+        aria-describedby="display-name-validation-feedback"
+        placeholder=""
+        required
+      >
+      <label for="displayName">Display Name</label>
+      <div
+        v-if="fields.displayName.isValid === false"
+        id="display-name-validation-feedback"
+        class="invalid-feedback"
+        v-html="fields.displayName.errorMessage">
+      </div>
     </div>
 
+    <div class="form-floating mb-3">
+      <input
+        name="email"
+        type="email"
+        class="form-control"
+        :class="[ fields.email.isValid === null ? '' : (fields.email.isValid ? 'is-valid' : 'is-invalid') ]"
+        aria-describedby="email-validation-feedback"
+        placeholder=""
+        required
+      >
+      <label for="email">Email address</label>
+      <div
+        v-if="fields.email.isValid === false"
+        id="email-validation-feedback"
+        class="invalid-feedback"
+        v-html="fields.email.errorMessage">
+      </div>
+    </div>
 
-    <button class="btn btn-primary w-100 py-2 my-3" type="submit">Create Account</button>
+    <div class="form-floating mb-3">
+      <input
+        name="password"
+        type="password"
+        class="form-control"
+        :class="[ fields.password.isValid === null ? '' : (fields.password.isValid ? 'is-valid' : 'is-invalid') ]"
+        aria-describedby="password-validation-feedback"
+        placeholder=""
+        required
+      >
+      <label for="password">Password</label>
+      <div
+          v-if="fields.password.isValid === false"
+        id="password-validation-feedback"
+        class="invalid-feedback"
+        v-html="fields.password.errorMessage">
+      </div>
+    </div>
+
+    <button :disabled="isLoading" class="btn btn-primary w-100 py-2 mb-3" type="submit">Create Account</button>
+    <div v-if="isLoading" class="spinner-border text-primary" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+
   </form>
 </template>
