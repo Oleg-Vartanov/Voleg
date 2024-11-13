f373bcc2d1bb45a983370f796e272a8f41448d12
Oleg Vartanov <olegvartanov1997@gmail.com>
2024-11-14 02:03:14 +0400

    F: Fixed small form toggle bug.

--- frontend/src/components/AuthForms.vue
+++ frontend/src/components/AuthForms.vue
@@ -1,24 +1,37 @@
 <script setup lang="ts">
-import {useRouter} from 'vue-router'
+import {useRoute, useRouter} from 'vue-router'
 
 const router = useRouter();
+const route = useRoute();
 </script>
 
 <template>
   <div class="ov-center">
     <main class="form-signin w-100 m-auto">
       <div class="btn-group mb-3 w-100" role="group" aria-label="Basic radio toggle button group">
+
         <input
           @click="router.push({ name: 'signIn' })"
-          type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-        <label class="btn btn-outline-primary" for="btnradio1">Sign In</label>
+          :checked="route.name === 'signIn'"
+          type="radio"
+          class="btn-check"
+          id="sign-in"
+        >
+        <label class="btn btn-outline-primary" for="sign-in">Sign In</label>
 
-        <input @click="router.push({ name: 'signUp' })"
-               type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-        <label class="btn btn-outline-primary" for="btnradio2">Sign Up</label>
+        <input
+          @click="router.push({ name: 'signUp' })"
+          :checked="route.name === 'signUp'"
+          type="radio"
+          class="btn-check"
+          id="sign-up"
+        >
+        <label class="btn btn-outline-primary" for="sign-up">Sign Up</label>
+
       </div>
 
       <router-view></router-view>
+
     </main>
   </div>
 </template>
