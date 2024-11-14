c20a0d4980c0448d51469d9b10b808ab3f89e847
Oleg Vartanov <olegvartanov1997@gmail.com>
2024-11-14 22:49:59 +0400

    F: Dynamic title.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -14,21 +14,25 @@ const index = createRouter({
         {
           path: '/',
           name: 'home',
+          meta: { title: 'Home' },
           component: () => import('../views/HomeView.vue'),
         },
         {
           path: '/about',
           name: 'about',
+          meta: { title: 'About' },
           component: () => import('../views/AboutView.vue'),
         },
         {
           path: '/pricing',
           name: 'pricing',
+          meta: { title: 'Pricing' },
           component: () => import('../views/PricingView.vue'),
         },
         {
           path: '/profile',
           name: 'profile',
+          meta: { title: 'Profile' },
           beforeEnter: [guards.isAuthenticated],
           component: () => import('../views/ProfileView.vue'),
         },
@@ -40,22 +44,32 @@ const index = createRouter({
             {
               path: '/sign-in',
               name: 'signIn',
+              meta: { title: 'Sign In' },
               component: () => import('../views/SignInView.vue'),
             },
             {
               path: '/sign-up',
               name: 'signUp',
+              meta: { title: 'Sign Up' },
               component: () => import('../views/SignUpView.vue'),
             },
           ]
         },
         {
           path: "/:notFound",
+          meta: { title: 'Not Found' },
           component: () => import('../views/NotFoundView.vue'),
         },
       ],
     },
   ]
 })
+
+// Add the beforeEach guard for updating document title and description
+index.beforeEach((to) => {
+  const { title } = to.meta;
+  const defaultTitle = 'voleg';
+  document.title = title || defaultTitle;
+});
 
 export default index
