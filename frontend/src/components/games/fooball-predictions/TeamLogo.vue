e9fa776a6ac4a4dcd1637f6ccd76f65f64d06f50
Oleg Vartanov <olegvartanov1997@gmail.com>
2024-11-24 20:12:40 +0400

    F: Prediction modal. Table style.

---
+++ frontend/src/components/games/fooball-predictions/TeamLogo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps(['teamName'])
+
+function getTag(name: string) {
+  return name.toLowerCase().replace(' ', '-');
+}
+</script>
+
+<template>
+  <img :src="'/team-logo/'+getTag(teamName)+'.png'"
+       width="20" height="20"
+       alt="logo">
+</template>
--- frontend/src/views/games/FootballPredictions.vue
+++ frontend/src/views/games/FootballPredictions.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import Client from '@/modules/api-client.ts';
 import { useTopAlerts } from "@/modules/top-alerts";
 import { Alert } from "@/models/alert";
+import TeamLogo from "@/components/games/fooball-predictions/TeamLogo.vue";
 
 const topAlerts = useTopAlerts();
 
@@ -46,37 +47,56 @@ function updateFixturesTable() {
 }
 updateFixturesTable();
 
+function predictionHomeScore(fixture) {
+  return fixture?.fixturePredictions?.homeScore == null ? '-' : fixture.fixturePredictions.homeScore;
+}
+function predictionAwayScore(fixture) {
+  return fixture?.fixturePredictions?.awayScore == null ? '-' : fixture.fixturePredictions.awayScore;
+}
 </script>
 
 <template>
   <div class="ov-center">
-    <div class="container text-center">
+    <div class="container">
 
       <div class="d-flex justify-content-center">
-        <div class="row align-items-center">
+        <div class="row">
+
+          <div class="col-auto mb-2 p-1">
+            <button v-if="!isLoadingFixtures"
+                    type="button"
+                    class="btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#predictionsModal"
+            >Make Predictions</button>
+          </div>
+
           <div class="col-auto mb-2 p-1">
             <div class="input-group">
               <span class="input-group-text">Start</span>
               <input id="start" class="form-control" type="date" v-model="start"/>
             </div>
           </div>
+
           <div class="col-auto mb-2 p-1">
             <div class="input-group">
               <span class="input-group-text">End</span>
               <input id="end" class="form-control" type="date" v-model="end"/>
             </div>
           </div>
+
           <div class="col-auto mb-2 p-1">
             <button @click="updateFixturesTable" class="btn btn-outline-primary" type="button">Filter</button>
           </div>
         </div>
+
       </div>
 
       <div v-if="isLoadingFixtures || isLoadingLeaderboard" class="spinner-border text-primary mt-3" role="status">
         <span class="visually-hidden">Loading...</span>
       </div>
 
-      <div class="row align-items-start">
+      <div class="row">
         <div class="col">
           <table v-if="!isLoadingFixtures" class="table">
             <thead>
@@ -89,15 +109,21 @@ updateFixturesTable();
             </thead>
             <tbody>
             <tr v-for="fixture in fixtures">
-              <td>
+              <td class="text-start">
+                <span>
+                  <TeamLogo :teamName="fixture.homeTeam.name"></TeamLogo>
                 {{ fixture.homeTeam.name }}
+                </span>
                 <br>
-                {{ fixture.awayTeam.name }}
+                <span>
+                  <TeamLogo :teamName="fixture.awayTeam.name"></TeamLogo>
+                  {{ fixture.awayTeam.name }}
+                </span>
               </td>
               <td>
-                {{ fixture?.fixturePredictions?.homeScore == null ? '-' : fixture.fixturePredictions.homeScore }}
+                {{ predictionHomeScore(fixture) }}
                 <br>
-                {{ fixture?.fixturePredictions?.awayScore == null ? '-' : fixture.fixturePredictions.awayScore }}
+                {{ predictionAwayScore(fixture) }}
               </td>
               <td>
                 {{ fixture.homeScore === null ? '-' : fixture.homeScore }}
@@ -134,7 +160,74 @@ updateFixturesTable();
         </div>
 
       </div>
-
     </div>
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="predictionsModal"
+      tabindex="-1"
+      data-bs-backdrop="static" data-bs-keyboard="false"
+      aria-labelledby="predictionsModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="predictionsModalLabel">Predictions</h1>
+          </div>
+          <div class="modal-body">
+
+            <table v-if="!isLoadingFixtures" class="table">
+              <thead>
+              <tr>
+                <th scope="col">Match</th>
+                <th scope="col" style="width: 20%; min-width: 80px;">Home</th>
+                <th scope="col" style="width: 20%; min-width: 80px;">Away</th>
+              </tr>
+              </thead>
+              <tbody>
+              <template v-for="fixture in fixtures">
+                <tr v-if="fixture.status !== 'finished'">
+                  <td class="text-start">
+                    <span>
+                      <TeamLogo :teamName="fixture.homeTeam.name"></TeamLogo>
+                    {{ fixture.homeTeam.name }}
+                    {{ fixture.status }}
+                    </span>
+                    <br>
+                    <span>
+                      <TeamLogo :teamName="fixture.awayTeam.name"></TeamLogo>
+                    {{ fixture.awayTeam.name }}
+                    </span>
+                  </td>
+                  <td>
+                    <input type="number"
+                           min="0" max="99"
+                           class="form-control"
+                           :value="predictionHomeScore(fixture)">
+                  </td>
+                  <td>
+                    <input type="number"
+                           min="0" max="99"
+                           class="form-control"
+                           :value="predictionAwayScore(fixture)">
+                  </td>
+                </tr>
+              </template>
+              </tbody>
+            </table>
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
+
+<style scoped></style>
