4831e1f14b21a9447ee2842242eac03eb6355c0e
Oleg Vartanov <olegvartanov1997@gmail.com>
2024-12-12 01:39:22 +0400

    Predictions import.

---
+++ backend/src/Command/ImportResultsCommand.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\FixturePrediction;
+use App\Repository\FixturePredictionRepository;
+use App\Repository\FixtureRepository;
+use App\Repository\TeamRepository;
+use App\Repository\UserRepository;
+use App\Service\CsvReader;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+#[AsCommand(name: 'import:predictions')]
+class ImportResultsCommand extends Command
+{
+    public function __construct(
+        private CsvReader $csvReader,
+        private KernelInterface $kernel,
+        private FixturePredictionRepository $fixturePredictionRepository,
+        private UserRepository $userRepository,
+        private TeamRepository $teamRepository,
+        private FixtureRepository $fixtureRepository,
+        private EntityManagerInterface $entityManager,
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $oleg = $this->userRepository->findOneBy(['displayName' => 'Voleg']) ?? throw new Exception();
+        $artem = $this->userRepository->findOneBy(['displayName' => 'Artem']) ?? throw new Exception();
+
+        $folder = $this->kernel->getProjectDir().'/data/';
+        $rows = $this->csvReader->read($folder.'fixtures.csv');
+        $headers = [
+            'home_team_id' => 0,
+            'away_team_id' => 1,
+            'artem_home_score' => 2,
+            'artem_away_score' => 3,
+            'oleg_home_score' => 4,
+            'oleg_away_score' => 5,
+        ];
+
+        unset($rows[0]);
+
+        $progressBar = new ProgressBar($output, count($rows));
+        $progressBar->start();
+
+        foreach ($rows as $row) {
+            if ($row[0] === null) continue;
+
+            $homeTeam = $this->teamRepository->findOneBy(['id' => $row[$headers['home_team_id']]]) ?? throw new Exception($row[$headers['home_team_id']]);
+            $awayTeam = $this->teamRepository->findOneBy(['id' => $row[$headers['away_team_id']]]) ?? throw new Exception($row[$headers['away_team_id']]);
+            $fixture = $this->fixtureRepository->findOneBy(['homeTeam' => $homeTeam, 'awayTeam' => $awayTeam]) ?? throw new Exception();
+
+            // Artem's prediction.
+            $fixturePrediction = $this->fixturePredictionRepository->findOneBy(['fixture' => $fixture, 'user' => $artem]);
+            if ($fixturePrediction === null) {
+                $fixturePrediction = new FixturePrediction();
+                $fixturePrediction->setFixture($fixture);
+                $fixturePrediction->setUser($artem);
+            }
+            $fixturePrediction->setHomeScore($row[$headers['artem_home_score']]);
+            $fixturePrediction->setAwayScore($row[$headers['artem_away_score']]);
+            $this->entityManager->persist($fixturePrediction);
+
+            // Oleg's prediction.
+            $fixturePrediction = $this->fixturePredictionRepository->findOneBy(['fixture' => $fixture, 'user' => $oleg]);
+            if ($fixturePrediction === null) {
+                $fixturePrediction = new FixturePrediction();
+                $fixturePrediction->setFixture($fixture);
+                $fixturePrediction->setUser($oleg);
+            }
+            $fixturePrediction->setHomeScore($row[$headers['oleg_home_score']]);
+            $fixturePrediction->setAwayScore($row[$headers['oleg_away_score']]);
+            $this->entityManager->persist($fixturePrediction);
+
+            $progressBar->advance();
+        }
+
+        $this->entityManager->flush();
+        $progressBar->finish();
+        $output->writeln(' Populated country table');
+
+        return Command::SUCCESS;
+    }
+}
