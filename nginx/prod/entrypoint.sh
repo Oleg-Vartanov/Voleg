5654ca53295addf5e26a96adb9395834df37556c
Oleg Vartanov <olegvartanov1997@gmail.com>
2025-03-22 03:32:57 +0400

    Improve entrypoint, fix issues.

--- nginx/prod/entrypoint.sh
+++ nginx/prod/entrypoint.sh
@@ -8,15 +8,18 @@ if [ ! -f /etc/letsencrypt/live/${DOMAIN}/fullchain.pem ]; then
   echo "Generating SSL certificate for ${DOMAIN}..."
 
   # Add ACME challenge location to the default config before template is used.
-  echo "Adding ACME challenge location to default Nginx config..."
   echo "server {
     listen 80; server_name ${DOMAIN} ${DOMAIN_API};
     location /.well-known/acme-challenge/ { root /var/www/certbot; allow all; }
     location / { return 404; }
   }" > /etc/nginx/conf.d/default.conf
 
+  mkdir -p /var/www/certbot
+  chown -R nginx:nginx /var/www/certbot
+  chmod -R 755 /var/www/certbot
+
   # Start Nginx in the background
-  nginx
+  nginx -g "daemon off;" &
 
   # Wait for Nginx to be ready by checking port 80
   until nc -z ${DOMAIN} 80; do
@@ -24,14 +27,15 @@ if [ ! -f /etc/letsencrypt/live/${DOMAIN}/fullchain.pem ]; then
     sleep 2
   done
 
-  mkdir -p /var/www/certbot
   certbot certonly --webroot -w /var/www/certbot -d ${DOMAIN} -d ${DOMAIN_API} --email ${CERTBOT_EMAIL} --agree-tos --non-interactive;
 
   nginx -s stop
+
+  # Wait for Nginx to stop completely
+  while pgrep -x nginx >/dev/null; do
+    echo "Waiting for Nginx to stop..."
+    sleep 2
+  done
 fi;
 
-# Starting crone
-crond
-
-# Run the original image entrypoint
-exec /docker-entrypoint.sh "$@"
+crond -f & exec /docker-entrypoint.sh "$@"
