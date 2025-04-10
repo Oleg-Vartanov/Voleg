65b45caa5566e2cbda37d2b0ac013d9315ca2f9c
Oleg Vartanov <olegvartanov1997@gmail.com>
2025-04-10 16:48:54 +0400

    Change scripts from CRLF to LF. To fix exec errors.

--- node/command.sh
+++ node/command.sh
@@ -9,4 +9,4 @@ openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 \
   -nodes -keyout /certificates/${DOMAIN}.key -out /certificates/${DOMAIN}.crt \
   -subj "/CN=${DOMAIN}" -addext "subjectAltName=DNS:${DOMAIN},DNS:*.${DOMAIN},IP:10.0.0.1"
 
-tail -f /dev/null
+npm run dev -- --host
