a8fc3effd4188c3f25476038531bc8f82bad49e8
Oleg Vartanov <olegvartanov1997@gmail.com>
2025-08-10 21:07:32 +0400

    added fixture_predictions transport

--- backend/src/FixturePredictions/Messenger/CalculatePointsMessage.php
+++ backend/src/FixturePredictions/Messenger/CalculatePointsMessage.php
@@ -2,6 +2,9 @@
 
 namespace App\FixturePredictions\Messenger;
 
+use Symfony\Component\Messenger\Attribute\AsMessage;
+
+#[AsMessage('fixture_predictions')]
 readonly class CalculatePointsMessage
 {
     public function __construct(
